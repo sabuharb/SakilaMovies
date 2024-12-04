9038894616902b4cc7b71ec21468347f2918b754
sabuharb <abuharb.sandy@gmail.com>
2024-12-04 13:47:47 -0600

    Initial commit for SakilaMovies, SakilaDataManager class coded and Actor Class too

--- src/main/java/com/pluralsight/Actor.java
+++ src/main/java/com/pluralsight/Actor.java
@@ -1,1 +1,30 @@
+public class Actor {
+    private int actorId;
+    private String firstName;
+    private String lastName;
+
+    public Actor(int actorId, String firstName, String lastName) {
+        this.actorId = actorId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // Getters
+    public int getActorId() {
+        return actorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Actor [actorId=" + actorId + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+    }
+}
 
